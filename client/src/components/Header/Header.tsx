8e0f68554571b7721637a167aa0a761aca1c136a
Yasir Tobbileh <yasur96@gmail.com>
2020-12-31 01:18:18 +0200

    feat/ add a header component

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+            <p className="header-disc">yasir.tobbileh@gmail.com</p>
+            <p className="header-disc">My name is Yasir</p>
+            <h2 className="header-disc">Full Stack Software Engineer</h2>
+            {/* <button id="btn-projects">Projects</button> */}
+
+        </div>
+      );
+}
+ 
+export default Header;
