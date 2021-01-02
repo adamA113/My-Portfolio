3eaa6c696a49d99bc36578afcfb9335c0efb6213
Yasir Tobbileh <yasur96@gmail.com>
2021-01-02 14:57:00 +0200

    feat/ create usedtech component

---
+++ client/src/components/GetInTouch/GetInTouch.tsx
@@ -0,0 +1,35 @@
+
+import './GetInTouch.css';
+const GetInTouch = () => {
+
+    const handleSubmit = () =>{
+
+    }
+    return ( 
+        <div id="query">
+            <form id="query-form" onSubmit={handleSubmit}>
+                <h3>Get In Touch</h3>
+                <br />
+                <div className="column">
+                    <label htmlFor="username" >User Name:</label>
+                    <input type="text" className="text" id="username" name="username" />
+                    <div className="username error" ></div>
+
+                    <label htmlFor="email" >Email:</label>
+                    <input type="email" className="text" id="email" name="email"/>
+                    <div className="email error" ></div>
+
+                    <label htmlFor="subject" >Subject:</label>
+                    <input type="text" className="text" id="subject" name="subject" /><br/>
+
+                    <label htmlFor="message" >Message:</label>
+                    <textarea className="text" id="message" name="message"></textarea>
+                </div>
+                <br />
+                <button className="button">Send Enquiry</button><br />
+            </form>
+        </div>
+     );
+}
+ 
+export default GetInTouch;
