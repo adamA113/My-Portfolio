1d55b042f2c057b96df422f9a8fd9653afd0bf46
Yasir Tobbileh <yasur96@gmail.com>
2021-01-02 15:00:15 +0200

    feat/ create usedtech component

---
+++ client/src/components/UsedTech/UsedTech.tsx
@@ -0,0 +1,88 @@
+
+import './UsedTech.css';
+const UsedTech = () => {
+    return (
+        <div className="tech">
+            <h3>Technologies I used</h3>
+            <div className="tech-container">
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/html-5.png" />
+                    <p>HTML5</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/css3.png" />
+                    <p>CSS3</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/sass.png" />
+                    <p>Sass</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/javascript.png" />
+                    <p>Javascript</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/typescript.png" />
+                    <p>Typescript</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/python.png" />
+                    <p>Python</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/django.png" />
+                    <p>Django</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/travis-ci.png" />
+                    <p>Travis CI</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/react-native.png" />
+                    <p>React</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/redux.png" />
+                    <p>Redux</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/mongodb.png" />
+                    <p>MongoDB</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/postgreesql.png" />
+                    <p>PostgreSQL</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://i.imgur.com/ri2cvJd.png" style={{"width": "48px", "height": "48px"}} />
+                    <p>Docker</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/kubernetes.png" />
+                    <p>kubernetes</p>
+                </div>
+
+                <div className="tech-item">
+                    <img src="https://img.icons8.com/color/48/000000/heroku.png" />
+                    <p>Heroku</p>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default UsedTech;
