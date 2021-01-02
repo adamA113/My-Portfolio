fc1c3424a1985f26db6444f7d6e69ae5e93d3b7b
Yasir Tobbileh <yasur96@gmail.com>
2021-01-02 18:05:55 +0200

    feat/ make the footer responsive

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -10,13 +10,24 @@ const Footer = () => {
                 <h4>Yasir Tobbileh</h4>
                 <p> Full Stack Software Engineer</p>
             </div>
-            
+
             <div className="footer-items">
-                <a className="active" href="#">Home</a>
-                <a href="#" className="fot-item">Yasir</a>
-                <a href="#" className="fot-item">Contact</a>
-                <a href="#" className="fot-item">Projects</a>
-                <a href="#" className="fot-item">About</a>
+                <div className="main-items">
+                    <a href="#" className="fot-item">Yasir</a>
+                    <a href="#" className="fot-item">Projects</a>
+                    <a href="#" className="fot-item">Contact</a>
+                    <a href="#" className="fot-item">About</a>
+                </div>
+                <div className="soc-icon">
+                    <a href="https://www.linkedin.com/in/yasir-tobbileh/" target="_blank" className="fot-item"><i className="fab fa-linkedin fa-2x"></i></a>
+
+                    <a href="https://www.linkedin.com/in/yasir-tobbileh/" target="_blank" className="fot-item"><i className="fab fa-facebook-square fa-2x"></i></a>
+                </div>
+
+            </div>
+
+            <div className="copyright">
+                <i className="far fa-copyright"> 2020 Designed by Yasir Tobbileh</i>
             </div>
         </div>
     );
