29934705053a66e296ad6e5f346e1f357133cfdf
Yasir Tobbileh <yasur96@gmail.com>
2021-01-17 17:34:05 +0200

    feat/ add hotlink to my email

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,13 +3,15 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <p className="header-disc">yasir.tobbileh@gmail.com</p>
+            <a className="header-email" href="mailto:yasir.tobbileh@gmail.com">
+                <p>yasir.tobbileh@gmail.com</p>
+            </a>
             <p className="header-disc">My name is Yasir</p>
             <h2 className="header-disc">Full Stack Software Engineer</h2>
             {/* <button id="btn-projects">Projects</button> */}
 
         </div>
-      );
+    );
 }
- 
+
 export default Header;
