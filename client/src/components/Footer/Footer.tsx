fc9aaf60b89efe9755b29b4829107f1ada079c12
Yasir Tobbileh <yasur96@gmail.com>
2021-01-17 17:24:22 +0200

    feat/ add github link

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -13,21 +13,21 @@ const Footer = () => {
 
             <div className="footer-items">
                 <div className="main-items">
-                    <a href="#" className="fot-item">Yasir</a>
-                    <a href="#" className="fot-item">Projects</a>
-                    <a href="#" className="fot-item">Contact</a>
-                    <a href="#" className="fot-item">About</a>
+                    <a href="/tech" className="fot-item">Tech.</a>
+                    <a href="/projects" className="fot-item">Projects</a>
+                    <a href="/contact" className="fot-item">Contact</a>
+                    <a href="/about" className="fot-item">About</a>
                 </div>
                 <div className="soc-icon">
                     <a href="https://www.linkedin.com/in/yasir-tobbileh/" target="_blank" className="fot-item"><i className="fab fa-linkedin fa-2x"></i></a>
 
-                    <a href="https://www.linkedin.com/in/yasir-tobbileh/" target="_blank" className="fot-item"><i className="fab fa-facebook-square fa-2x"></i></a>
+                    <a href="https://github.com/adamA113" target="_blank" className="fot-item"><i className="fab fa-github fa-2x"></i></a>
                 </div>
 
             </div>
 
             <div className="copyright">
-                <i className="far fa-copyright"> 2020 Designed by Yasir Tobbileh</i>
+                <i className="far fa-copyright"> 2021 Designed by Yasir Tobbileh</i>
             </div>
         </div>
     );
