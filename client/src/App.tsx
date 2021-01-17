8bc99e9a57d78f88ae5a802795bc9e7e4307dc93
Yasir Tobbileh <yasur96@gmail.com>
2021-01-17 17:36:39 +0200

    refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import GetInTouch from './components/GetInTouch/GetInTouch';
+import UsedTech from './components/UsedTech/UsedTech';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/" component={Header} />
+          {/* <Route path="/tech" component={UsedTech} /> */}
+          <Route path="/contact" component={GetInTouch} />
+          <Route path="/projects" component={Projects} />
+
+        </Switch>
+        <UsedTech />
+        <Footer />
+
+
+
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ client/src/components/About/About.tsx
@@ -0,0 +1,23 @@
+import './About.css';
+const About = () => {
+    return (
+        <div>
+            <div className="project-info">
+                <h3>My Latest Projects</h3>
+
+                <div>
+                    <img className="project-img" src="servize.png" />
+
+                    <div className="project-disc">
+                        <p>Servize</p>
+                        Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default About;
