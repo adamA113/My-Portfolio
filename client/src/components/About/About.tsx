1ac85f228d3a6ab150bbab398aaf4181ce5a7c28
Yasir Tobbileh <yasur96@gmail.com>
2021-01-19 13:42:40 +0200

    feat/ add about me section

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -1,17 +1,19 @@
-import './About.css';
+// import './About.css';
 const About = () => {
     return (
         <div>
             <div className="project-info">
-                <h3>My Latest Projects</h3>
+                <h3>About me</h3>
 
                 <div>
-                    <img className="project-img" src="servize.png" />
+                    {/* <img className="project-img" src="servize.png" /> */}
 
                     <div className="project-disc">
-                        <p>Servize</p>
-                        Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.
+                        <p>My name is Yasir Tobbileh. I am a Full stack developer/ Software Engineer. I am interested in web development and network design.</p>
+
+                        <p>Also, as an Electrical Engineer, I am really into creating software for IoT devices and embedded systems, as well as designing and implementing their hardware as well. </p>
                     </div>
+                    <a className="query-btn" href="https://www.visualcv.com/pdfs/6311737/" target="_blank">My Resume</a><br />
 
                 </div>
             </div>
