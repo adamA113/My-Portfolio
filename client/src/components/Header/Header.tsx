c68ed6d136156eb18caacdcca54cbe4935755cc6
Yasir Tobbileh <yasur96@gmail.com>
2021-01-20 00:42:30 +0200

    feat/ add header background img

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,12 +3,17 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <a className="header-email" href="mailto:yasir.tobbileh@gmail.com">
+            {/* <a className="header-email" href="mailto:yasir.tobbileh@gmail.com">
                 <p>yasir.tobbileh@gmail.com</p>
-            </a>
-            <p className="header-disc">My name is Yasir</p>
-            <h2 className="header-disc">Full Stack Software Engineer</h2>
-            {/* <button id="btn-projects">Projects</button> */}
+            </a> */}
+            {/* <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60" /> */}
+
+            <div className="my-container">
+                <h1 className="header-disc">Hi, I'm Yasir Tobbileh</h1>
+                <p className="header-disc">Full Stack Software Engineer</p>
+                {/* <button id="btn-projects">Projects</button> */}
+            </div>
+            
 
         </header>
     );
