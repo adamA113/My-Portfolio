f5ccbb9f20a83e9f3d888cc4dd7c82b14e07b6d3
Yasir Tobbileh <yasur96@gmail.com>
2021-01-19 13:37:46 +0200

    refactor/ change root html tag to header tag

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@
 import './Header.css';
 const Header = () => {
     return (
-        <div className="header">
+        <header className="header">
             <a className="header-email" href="mailto:yasir.tobbileh@gmail.com">
                 <p>yasir.tobbileh@gmail.com</p>
             </a>
@@ -10,7 +10,7 @@ const Header = () => {
             <h2 className="header-disc">Full Stack Software Engineer</h2>
             {/* <button id="btn-projects">Projects</button> */}
 
-        </div>
+        </header>
     );
 }
 
