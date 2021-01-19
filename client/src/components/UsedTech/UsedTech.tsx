333dc82ecb6adb9eee478ec123668787fd7eb83c
Yasir Tobbileh <yasur96@gmail.com>
2021-01-19 13:40:39 +0200

    feat/ add skill-bar for each tech.

--- client/src/components/UsedTech/UsedTech.tsx
+++ client/src/components/UsedTech/UsedTech.tsx
@@ -5,74 +5,145 @@ const UsedTech = () => {
         <div className="tech">
             <h3>Technologies I used</h3>
             <div className="tech-container">
+
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/html-5.png" />
                     <p>HTML5</p>
+
+                    <div className="skills-container">
+                        <div className="skills html5"></div>
+                        <span className="tooltiptext">85%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/css3.png" />
                     <p>CSS3</p>
+
+                    <div className="skills-container">
+                        <div className="skills css3"></div>
+                        <span className="tooltiptext">85%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/sass.png" />
                     <p>Sass</p>
+
+                    <div className="skills-container">
+                        <div className="skills sass"></div>
+                        <span className="tooltiptext">70%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/javascript.png" />
                     <p>JavaScript</p>
+
+                    <div className="skills-container">
+                        <div className="skills javascript"></div>
+                        <span className="tooltiptext">85%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/typescript.png" />
                     <p>TypeScript</p>
+
+                    <div className="skills-container">
+                        <div className="skills typescript"></div>
+                        <span className="tooltiptext">65%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" />
                     <p>C++</p>
+                    
+                    <div className="skills-container">
+                        <div className="skills cplus"></div>
+                        <span className="tooltiptext">55%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/python.png" />
                     <p>Python</p>
+
+                    <div className="skills-container">
+                        <div className="skills python"></div>
+                        <span className="tooltiptext">60%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/django.png" />
                     <p>Django</p>
+
+                    <div className="skills-container">
+                        <div className="skills django"></div>
+                        <span className="tooltiptext">55%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/travis-ci.png" />
                     <p>Travis CI</p>
+
+                    <div className="skills-container">
+                        <div className="skills travis"></div>
+                        <span className="tooltiptext">60%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/react-native.png" />
                     <p>React</p>
+
+                    <div className="skills-container">
+                        <div className="skills react"></div>
+                        <span className="tooltiptext">85%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/redux.png" />
                     <p>Redux</p>
+
+                    <div className="skills-container">
+                        <div className="skills redux"></div>
+                        <span className="tooltiptext">85%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/mongodb.png" />
                     <p>MongoDB</p>
+
+                    <div className="skills-container">
+                        <div className="skills mongodb"></div>
+                        <span className="tooltiptext">75%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/postgreesql.png" />
                     <p>PostgreSQL</p>
+
+                    <div className="skills-container">
+                        <div className="skills postgresql"></div>
+                        <span className="tooltiptext">70%</span>
+                    </div>
                 </div>
 
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/docker.png" />
                     <p>Docker</p>
+
+                    <div className="skills-container">
+                        <div className="skills docker"></div>
+                        <span className="tooltiptext">60%</span>
+                    </div>
                 </div>
 
                 {/* <div className="tech-item">
@@ -83,6 +154,11 @@ const UsedTech = () => {
                 <div className="tech-item">
                     <img src="https://img.icons8.com/color/48/000000/heroku.png" />
                     <p>Heroku</p>
+
+                    <div className="skills-container">
+                        <div className="skills heroku"></div>
+                        <span className="tooltiptext">75%</span>
+                    </div>
                 </div>
 
             </div>
