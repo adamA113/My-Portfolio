edba2181a621437af942d1384e0b7854920a0301
Yasir Tobbileh <yasur96@gmail.com>
2021-01-20 00:45:59 +0200

    feat/ add my cell phone number

--- client/src/components/GetInTouch/GetInTouch.tsx
+++ client/src/components/GetInTouch/GetInTouch.tsx
@@ -2,12 +2,12 @@
 import './GetInTouch.css';
 const GetInTouch = () => {
 
-    const handleSubmit = () =>{
+    const handleSubmit = () => {
 
     }
-    return ( 
+    return (
         <div id="query">
-            <form id="query-form" onSubmit={handleSubmit}>
+            {/* <form id="query-form" onSubmit={handleSubmit}>
                 <h3>Get In Touch</h3>
                 <br />
                 <div className="column">
@@ -27,9 +27,31 @@ const GetInTouch = () => {
                 </div>
                 <br />
                 <button className="query-btn">Send Enquiry</button><br />
-            </form>
+            </form> */}
+            <div className="contact-container">
+                <div className="contact-details">
+                    <h3>Get In Touch</h3>
+                    <div className="email-container">
+                        <p> Email:
+                            <a className="my-email" href="mailto:yasir.tobbileh@gmail.com">
+                                <p>yasir.tobbileh@gmail.com</p>
+                            </a>
+                        </p>
+
+                    </div>
+
+                    <div className="phone-container">
+                        <span>Cell Phone:</span>
+                        <p className="my-phone"> (+972) 59 247 4285</p>
+                    </div>
+                </div>
+
+                <div className="contact-img">
+                    <img src="https://images.unsplash.com/photo-1532262318931-eb6ff23aabc6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=662&q=80" />
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default GetInTouch;
