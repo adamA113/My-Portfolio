034864bccdd702ac3a1b099ebe03fa1b489039d9
Yasir Tobbileh <yasur96@gmail.com>
2021-01-20 19:51:20 +0200

    feat/ add github link and deployment link

--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -12,12 +12,12 @@ const Projects = () => {
                 <div className="project-container">
                     <h4>Servize</h4>
                     <div className="project-disc">
-                        <p>Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.</p>
+                        <p>A website that allows you to search for a certain service and find a potential provider and give you the best to choose from.</p>
                         <p><b>Role:</b> Scrum Master, Front End Developer.</p>
                     </div>
 
+                    <p><b>Stack:</b></p>
                     <div className="project-stack">
-                        <p><b>Stack:</b></p>
                         <div className="stack-item">
                             <img src="https://img.icons8.com/color/48/000000/django.png" />
                             <p>Django</p>
@@ -50,7 +50,7 @@ const Projects = () => {
                     </div>
 
                     <div className="project-links">
-                        <a className="project-github" href="">Source Code</a>
+                        <a className="project-github" href="https://github.com/adamA113/servize.git" target="_blank">Source Code</a>
                     </div>
                 </div>
             </div>
@@ -64,12 +64,12 @@ const Projects = () => {
                 <div className="project-container">
                     <h4>HotelCom</h4>
                     <div className="project-disc">
-                        <p>Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.</p>
-                        <p><b>Role:</b> Scrum Master, Front End Developer.</p>
+                        <p>This website will help you to search for appropriate hotels in a specific city and let you filter the results based on the location or the price.</p>
+                        <p><b>Role:</b> Full-Stack Developer.</p>
                     </div>
 
+                    <p><b>Stack:</b></p>
                     <div className="project-stack">
-                        <p><b>Stack:</b></p>
                         <div className="stack-item">
                             <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" />
                             <p>Node.js</p>
@@ -92,7 +92,9 @@ const Projects = () => {
                     </div>
 
                     <div className="project-links">
-                        <a className="project-github" href="">Source Code</a>
+                        <a className="project-github" href="https://github.com/adamA113/GreenFieldLegacy.git" target="_blank">Source Code</a>
+
+                        <a className="project-deploy" href="https://legacy-hotels2.herokuapp.com/" target="_blank">Go to Website</a>
                     </div>
                 </div>
             </div>
