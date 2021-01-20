e3e78967802ff942737d59520ea291af4eb11160
Yasir Tobbileh <yasur96@gmail.com>
2021-01-20 16:42:06 +0200

    feat/ add my latest projects

--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,23 +1,104 @@
 import './Projects.css';
 const Projects = () => {
     return (
-        <div>
+        <div className="projects">
+            <h3>My Latest Projects</h3>
+
             <div className="project-info">
-                <h3>My Latest Projects</h3>
+                <div className="project-img">
+                    <img src="servize.png" />
+                </div>
 
-                <div>
-                    <img className="project-img" src="servize.png" />
-
+                <div className="project-container">
+                    <h4>Servize</h4>
                     <div className="project-disc">
-                        <p>Servize</p>
-                        Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.
+                        <p>Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.</p>
+                        <p><b>Role:</b> Scrum Master, Front End Developer.</p>
                     </div>
 
+                    <div className="project-stack">
+                        <p><b>Stack:</b></p>
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/django.png" />
+                            <p>Django</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/react-native.png" />
+                            <p>React</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/redux.png" />
+                            <p>Redux</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/sass.png" />
+                            <p>Sass</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/postgreesql.png" />
+                            <p>PostgreSQL</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/travis-ci.png" />
+                            <p>Travis CI</p>
+                        </div>
+                    </div>
+
+                    <div className="project-links">
+                        <a className="project-github" href="">Source Code</a>
+                    </div>
                 </div>
             </div>
-            
+
+
+            <div className="project-info">
+                <div className="project-img">
+                    <img src="hotelcom.png" />
+                </div>
+
+                <div className="project-container">
+                    <h4>HotelCom</h4>
+                    <div className="project-disc">
+                        <p>Website that allow you to search for a certain service and find a potentail providers and give you the best to choose from.</p>
+                        <p><b>Role:</b> Scrum Master, Front End Developer.</p>
+                    </div>
+
+                    <div className="project-stack">
+                        <p><b>Stack:</b></p>
+                        <div className="stack-item">
+                            <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" />
+                            <p>Node.js</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/react-native.png" />
+                            <p>React</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/redux.png" />
+                            <p>Redux</p>
+                        </div>
+
+                        <div className="stack-item">
+                            <img src="https://img.icons8.com/color/48/000000/mongodb.png" />
+                            <p>MongoDB</p>
+                        </div>
+                    </div>
+
+                    <div className="project-links">
+                        <a className="project-github" href="">Source Code</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
-      );
+    );
 }
- 
+
 export default Projects;
