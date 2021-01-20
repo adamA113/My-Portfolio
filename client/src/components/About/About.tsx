07b58fb28e2539bccf05289ad254707761ab3009
Yasir Tobbileh <yasur96@gmail.com>
2021-01-20 19:48:50 +0200

    feat/ change the about me description

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
 
                 <div className="about-me">
                     <h3>About me</h3>
-                    <p>My name is Yasir Tobbileh. I am a Full stack developer/ Software Engineer. I am interested in web development and network design.</p>
+                    <p>I am a Full-stack developer/ Software Engineer. I am interested in web development and network design, cloud, and virtualization.</p>
 
                     <p>Also, as an Electrical Engineer, I am really into creating software for IoT devices and embedded systems, as well as designing and implementing their hardware as well. </p>
 
