6d5b4b467c8702b8796f9de4547c8a7835b08cde
Yasir Tobbileh <yasur96@gmail.com>
2021-03-07 00:25:04 +0200

    refactor/ adding alt attribute to all images

--- client/src/components/UsedTech/UsedTech.tsx
+++ client/src/components/UsedTech/UsedTech.tsx
@@ -7,7 +7,7 @@ const UsedTech = () => {
             <div className="tech-container">
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/html-5.png" />
+                    <img src="https://img.icons8.com/color/48/000000/html-5.png" alt="HTML5 logo"/>
                     <p>HTML5</p>
 
                     <div className="skills-container">
@@ -17,7 +17,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/css3.png" />
+                    <img src="https://img.icons8.com/color/48/000000/css3.png" alt="CSS3 logo"/>
                     <p>CSS3</p>
 
                     <div className="skills-container">
@@ -27,7 +27,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/sass.png" />
+                    <img src="https://img.icons8.com/color/48/000000/sass.png" alt="Sass logo" />
                     <p>Sass</p>
 
                     <div className="skills-container">
@@ -37,7 +37,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/javascript.png" />
+                    <img src="https://img.icons8.com/color/48/000000/javascript.png" alt="JavaScript logo" />
                     <p>JavaScript</p>
 
                     <div className="skills-container">
@@ -47,7 +47,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/typescript.png" />
+                    <img src="https://img.icons8.com/color/48/000000/typescript.png" alt="TypeScript logo" />
                     <p>TypeScript</p>
 
                     <div className="skills-container">
@@ -57,7 +57,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" />
+                    <img src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.svg" alt="Node.js logo" />
                     <p>Node.js</p>
 
                     <div className="skills-container">
@@ -67,7 +67,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" />
+                    <img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" alt="C++ logo" />
                     <p>C++</p>
                     
                     <div className="skills-container">
@@ -77,7 +77,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/python.png" />
+                    <img src="https://img.icons8.com/color/48/000000/python.png" alt="Python logo" />
                     <p>Python</p>
 
                     <div className="skills-container">
@@ -87,7 +87,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/django.png" />
+                    <img src="https://img.icons8.com/color/48/000000/django.png" alt="Django logo" />
                     <p>Django</p>
 
                     <div className="skills-container">
@@ -97,7 +97,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/travis-ci.png" />
+                    <img src="https://img.icons8.com/color/48/000000/travis-ci.png" alt="Travis CI logo" />
                     <p>Travis CI</p>
 
                     <div className="skills-container">
@@ -107,7 +107,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/react-native.png" />
+                    <img src="https://img.icons8.com/color/48/000000/react-native.png" alt="React logo" />
                     <p>React</p>
 
                     <div className="skills-container">
@@ -117,7 +117,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/redux.png" />
+                    <img src="https://img.icons8.com/color/48/000000/redux.png" alt="Redux logo" />
                     <p>Redux</p>
 
                     <div className="skills-container">
@@ -127,7 +127,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/mongodb.png" />
+                    <img src="https://img.icons8.com/color/48/000000/mongodb.png" alt="MongoDB logo"/>
                     <p>MongoDB</p>
 
                     <div className="skills-container">
@@ -137,7 +137,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/postgreesql.png" />
+                    <img src="https://img.icons8.com/color/48/000000/postgreesql.png" alt="PostgreSQL logo" />
                     <p>PostgreSQL</p>
 
                     <div className="skills-container">
@@ -147,7 +147,7 @@ const UsedTech = () => {
                 </div>
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/docker.png" />
+                    <img src="https://img.icons8.com/color/48/000000/docker.png" alt="Docker logo"  />
                     <p>Docker</p>
 
                     <div className="skills-container">
@@ -162,7 +162,7 @@ const UsedTech = () => {
                 </div> */}
 
                 <div className="tech-item">
-                    <img src="https://img.icons8.com/color/48/000000/heroku.png" />
+                    <img src="https://img.icons8.com/color/48/000000/heroku.png" alt="Heroku logo" />
                     <p>Heroku</p>
 
                     <div className="skills-container">
